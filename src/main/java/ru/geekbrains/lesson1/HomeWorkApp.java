fb643b10d3c5e440b9e190e0c65ad59027eb9faf
Tatyana Sinichenkova <t.sinichenkova@infinnity.ru>
2021-10-01 22:38:58 +0500

    добавлено 1 дз

---
+++ src/main/java/ru/geekbrains/lesson1/HomeWorkApp.java
@@ -0,0 +1,54 @@
+package ru.geekbrains.lesson1;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class HomeWorkApp {
+    public static void main(String[] args) {
+        printThreeWords();
+        checkSumSign();
+        printColor();
+        compareNumbers();
+    }
+
+    public static void printThreeWords() {
+        String[] fruits = {"Orange", "Banana", "Apple"};
+        for (String fruit : fruits) {
+            System.out.println(fruit);
+        }
+    }
+
+    public static void checkSumSign() {
+        int a = 5;
+        int b = -11;
+        int c = a + b;
+        if (c > 0) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
+    }
+
+    public static void printColor() {
+        int value = 5;
+        if (value <= 0) {
+            System.out.println("Красный");
+        } else if (value > 0 && value <= 100) {
+            System.out.println("Желтый");
+        } else {
+            System.out.println("Зеленый");
+        }
+    }
+
+    public static void compareNumbers() {
+        int a = 5;
+        int b = 10;
+        if (a >= b) {
+            System.out.println("a >= b");
+        } else {
+            System.out.println("a < b");
+        }
+    }
+
+}
+
